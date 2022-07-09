6a85ca9b5dcf617757827850b257c66d47251cdc
RyanKruger1 <ryankruger45@gmail.com>
2022-07-09 17:29:52 +0200

    Add project files.

---
+++ piggy-bank/CommandLineClient.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace piggy_bank
+{
+    internal class CommandLineClient
+    {
+
+        private string _currentCoin;
+
+        public CommandLineClient()
+        {
+            Console.Title = "Piggy Bank";
+        }
+
+        public void greet()
+        {
+            Console.WriteLine("Welcome to your piggy bank");
+        }
+
+        public void showAmount(decimal amount)
+        {
+            Console.WriteLine("You have $ {0} in your piggy bank ", amount);
+        }
+
+        public int showMenu()
+        {
+            Console.WriteLine("Please select an option:");
+            Console.WriteLine("1) Add coin to your piggy bank.");
+            Console.WriteLine("2) See piggy bank cash amount.");
+            Console.WriteLine("3) Empty Piggy bank");
+           
+            try
+            {
+                int option = Int16.Parse(Console.ReadLine());
+                Console.Clear();
+                return option;
+            }
+            catch (FormatException ex)
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter a valid menu item number.");
+                return -1;
+            } 
+        }
+
+        public int AskCoin(IList<string> coins)
+        { 
+            Console.WriteLine("Please select which coin you would like to add:");
+            int counter = 1;
+            foreach(string c in coins)
+            {
+                Console.WriteLine("{0}) {1}", counter, c);
+                counter++;
+            }
+
+            try
+            {
+                int option = Int16.Parse(Console.ReadLine());
+                Console.Clear();
+                if (option > 5 || option <= 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please select a menu item by input their number");
+                    return -1;
+                }
+                _currentCoin = coins[option - 1];
+                return option;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please enter a valid menu item number.");
+                return -1;
+            }
+        }
+
+        public void printAmount(decimal amount)
+        {
+            Console.WriteLine("You current have {0} in your piggy bank" , amount);
+        }
+
+        public int getAmountCoins()
+        {
+            Console.WriteLine("Please enter the amount of {0}s you would like to insert.",_currentCoin);
+          
+            try
+            {
+                int option = Int16.Parse(Console.ReadLine());
+                Console.Clear();
+                if (option <= 0)
+                {
+                    Console.WriteLine("Please select a menu item by input their number");
+                    return -1;
+                }
+
+                return option;
+            }
+            catch (OverflowException ex)
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter a valid amount of coins");
+                return -1;
+            }
+            catch (FormatException ex)
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter a valid amount.");
+                return -1;
+            }
+        }
+
+    }
+}
---
+++ piggy-bank/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using piggy_bank;
+using piggy_bank.Enums;
+using piggy_bank.Interfaces;
+
+Boolean run_progam = true;
+CommandLineClient client = new CommandLineClient();
+PiggyBank bank = new PiggyBank();
+
+List<string> coinsName = CoinMetaData.getCoinInformation().Keys.ToList();
+
+client.greet();
+while (run_progam)
+{
+    client.showAmount(bank.GetTotalAmount());
+   
+    switch (client.showMenu())
+    {
+        case 1:
+
+            int selection = client.AskCoin(coinsName);
+
+            Coin coin = new Coin(coinsName[selection - 1]);
+
+            int amountCoins = client.getAmountCoins();
+
+            for (int i = 0; i < amountCoins ;i++)
+            {
+                if ( PiggyBankMetaData.PiggyBank.maxVolumeCMM - bank.totalVolume > 0)
+                {
+                    bank.AddCoin(coin);
+                }
+                else
+                {
+                    Console.WriteLine("Your piggy bank is full, you cannot add any more coins");
+                    break;
+                }
+            }
+
+            break; 
+        case 2:
+
+            client.printAmount(bank.GetTotalAmount());
+
+            break;
+        case 3:
+            bank.Reset();
+            break;
+        case 4:
+            // code block
+            break;
+    }
+
+}
